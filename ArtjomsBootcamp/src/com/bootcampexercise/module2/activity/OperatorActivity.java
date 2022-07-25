d2d66ddc72f4aa461ce515ded6ba02a066e9c3b0
Artjoms Smirnovs <smirnovsartyoms@gmail.com>
2022-07-25 16:41:43 +0300

    OperatorActivity

---
+++ ArtjomsBootcamp/src/com/bootcampexercise/module2/activity/OperatorActivity.java
@@ -0,0 +1,12 @@
+package com.bootcampexercise.module2.activity;
+
+public class OperatorActivity {
+public static void main (String[] args) {
+    int i = 20;
+    int j = 30;
+    int sum = i + j;
+    System.out.println(sum);
+
+
+}
+}
