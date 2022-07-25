2b2dfc5d479d99be96a985c1221ed55eb62a64a8
Artjoms Smirnovs <smirnovsartyoms@gmail.com>
2022-07-25 15:03:44 +0300

    VariableAssignment

---
+++ ArtjomsBootcamp/src/com/bootcampexercise/module2/activity/VariableAssignmentActivity.java
@@ -0,0 +1,9 @@
+package com.bootcampexercise.module2.activity;
+
+public class VariableAssignmentActivity {
+    public static void main(String[] args) {
+        int i = 20;
+        i = 30;
+        System.out.println(i);
+    }
+}
