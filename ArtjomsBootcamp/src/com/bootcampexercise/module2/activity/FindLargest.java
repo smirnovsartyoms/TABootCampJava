650f17d520eb5a66cf894c6d739b03fc59d86142
Artjoms Smirnovs <smirnovsartyoms@gmail.com>
2022-07-25 17:01:05 +0300

    FindLargest

---
+++ ArtjomsBootcamp/src/com/bootcampexercise/module2/activity/FindLargest.java
@@ -0,0 +1,14 @@
+package com.bootcampexercise.module2.activity;
+
+public class FindLargest {
+    public static void main (String[] args) {
+        int i = 20;
+        int j = 30;
+
+        if (i > j) {
+            System.out.println("The value of i is greater than j");
+        }else {
+            System.out.println("The value of j is greater than i");
+        }
+        }
+}
