c2f35457e5b03969074f132a1b176fe0eb5ca5f8
Artjoms Smirnovs <smirnovsartyoms@gmail.com>
2022-07-26 10:02:37 +0300

    PrintNumWithWhile

---
+++ ArtjomsBootcamp/src/com/bootcampexercise/module2/activity/PrintNumWithWhile.java
@@ -0,0 +1,14 @@
+package com.bootcampexercise.module2.activity;
+
+public class PrintNumWithWhile {
+    public static void main(String[] args) {
+
+        // Print all odd numbers less than 100
+        int i = 1;
+        while (i < 100) {
+            System.out.println(i);
+            i = i + 2;
+        }
+
+    }
+}
